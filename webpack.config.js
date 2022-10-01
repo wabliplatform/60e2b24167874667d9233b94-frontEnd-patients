cdd1260766ea2360f33d61835b2261031a03eaaa
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-10-01 11:52:28 +0300

    My commit message

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: {
 	'home' : './javascript/home.js',
 	'create' : './javascript/create.js',
+	'Page3' : './javascript/Page3.js',
 },
   output: {
     path: path.resolve(__dirname, 'dist'),
