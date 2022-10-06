ccb15bd2872884b46e641ac1c681d0496d120779
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-10-06 17:37:43 +0300

    My commit message

--- javascript/home.js
+++ javascript/home.js
@@ -190,6 +190,348 @@ let apiPatientApi = new TempApi.PatientApi();import TempApi from '../src/index';
           }
         });
       }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('insbn4').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("insbn4")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('ilch1i').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ilch1i")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('ikl6bq').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("ikl6bq")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('ia664z').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ia664z")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('iilmvj').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("iilmvj")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('i8v3by').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("i8v3by")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('ifbpwl').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("ifbpwl")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('ikue9n').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ikue9n")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('iaanam').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("iaanam")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('ijxxan').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ijxxan")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('ibdamf').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("ibdamf")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('ixy92x').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ixy92x")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('isgr0d').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("isgr0d")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('iyupmi').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("iyupmi")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('i31sib').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("i31sib")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('irhy8y').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("irhy8y")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};document.getElementById('isillx').onclick = (event) => {
+    event.preventDefault();
+    {  
+      let transitionId = window.location.href.split('/').at(-1);
+      let parentId = "";
+      const storedData = window.localStorage.getItem("data");
+      const newMap = new Map(JSON.parse(storedData));
+      newMap.forEach((value, key) => {
+        if (
+          document.getElementById(key) !== null &&
+          document
+            .getElementById(key)
+            .contains(document.getElementById("isillx")) === true &&
+            document.getElementById(key).contains(document.getElementById(parentId)) === false
+        ) {
+          transitionId = value._id;
+          parentId = key;
+        }
+      });
+     location.href= '/manage/' + transitionId;}};document.getElementById('ijssvh').onclick = (event) => {
+    event.preventDefault();
+    let patientId = window.location.pathname.replace('/home/','');
+      if(patientId === '/home' || patientId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ijssvh")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            patientId = value._id;
+            parentId = key;
+          }
+        });
+      }
     apiPatientApi.deletepatient( patientId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');}});};window.onload = () => {apiPatientApi.getAllpatient((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("ivs66").querySelectorAll( "[dataitem='true']" )].filter(
     (element, index, array) =>
     !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
