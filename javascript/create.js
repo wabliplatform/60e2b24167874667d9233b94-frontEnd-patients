f220a73a816f64e72299f4f68efff7ad1babb62e
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-10-11 10:11:00 +0300

    My commit message

--- javascript/create.js
+++ javascript/create.js
@@ -1,3 +1,16 @@
-let apiPatientApi = new TempApi.PatientApi();import TempApi from '../src/index';let patient = new TempApi.Patient();document.getElementById('i1xwyz').onclick = (event) => {
-    event.preventDefault();
-    patient['pname'] = document.querySelector("[annotationname = 'pname']").value;patient['lname'] = document.querySelector("[annotationname = 'lname']").value;patient['amka'] = document.querySelector("[annotationname = 'amka']").value;patient['mphone'] = document.querySelector("[annotationname = 'mphone']").value;apiPatientApi.createpatient( patient, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); {   location.href= '/home' ;}}});};window.onload = () => {};
+let apiPatientApi = new TempApi.PatientApi();import TempApi from '../src/index';window.onload = () => {apiPatientApi.getAllpatient((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("i1xwyz").querySelectorAll( "[dataitem='true']" )].filter(
+    (element, index, array) =>
+    !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
+  );const map = new Map();  data.forEach((item,i) => {
+    if(subDataElements.length > i)
+      {
+        console.log('There are no inside data elements');
+        map.set(subDataElements[i].getAttribute('id'), data[data.length-i-1])
+        
+      }
+      
+    });
+
+    window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));
+    
+    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
